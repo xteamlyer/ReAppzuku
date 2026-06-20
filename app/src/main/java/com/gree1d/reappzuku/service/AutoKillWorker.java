76f557cc0440e9af1083369aacfeea609d8d7cce
gree1d <161749879+gree1d@users.noreply.github.com>
2026-06-20 09:45:27 +0400

    Update AutoKillWorker.java

--- app/src/main/java/com/gree1d/reappzuku/service/AutoKillWorker.java
+++ app/src/main/java/com/gree1d/reappzuku/service/AutoKillWorker.java
@@ -20,7 +20,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-import com.gree1d.reappzuku.utils.ShellManager;
+import com.gree1d.reappzuku.core.ShellManager;
 import com.gree1d.reappzuku.manager.BackgroundAppManager;
 import com.gree1d.reappzuku.manager.AutoKillManager;
 
